d115c14061fdadd93b25acfb94ff1ce0bbb9581f
wassimztn <wassimztn@users.noreply.github.com>
2025-10-14 15:28:17 +0200

    Rectification LocalDate et demandeService

--- reservation-vehicule/src/main/java/com/example/reservation/DemandeService.java
+++ reservation-vehicule/src/main/java/com/example/reservation/DemandeService.java
@@ -1,8 +1,10 @@
 package com.example.reservation;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class DemandeService {
+
     private Gateway gateway;
 
     public DemandeService(Gateway gateway) {
@@ -10,9 +12,9 @@ public class DemandeService {
     }
 
     public boolean creerDemande(Personne personne, Type type, LocalDate dateDebut, int duree) {
-        // Crée un objet demande
-        Demande demande = new Demande(LocalDate.now(), numero:null, dateDebut, personne, type, vehicule:null, duree, dateretoureffectif:null, "demandé")
-        // Appelle la Gateway pour insérer dans la BDD
+        LocalDate dateReserv = LocalDate.now();
+        int numero = gateway.getNextNumero(dateReserv);
+        Demande demande = new Demande(dateReserv, numero, dateDebut, personne, type, null, duree, null, "demandée");
         return gateway.insertDemande(demande);
     }
 }
